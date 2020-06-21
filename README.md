5d70fd5d4e52de4fbf9d9aca4a3fa7dbea9e6e30
Nitzan Raz <nitz.raz@gmail.com>
2020-06-21 11:34:26 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Israeli court SGN extractor
+I got some attachment by mail that is an SGN file from the Israeli court system.  
+They require a Windows-only software to view the files.  
+This is unthinkable.  
+
+## Caveats
+* Doesn't actually verify the digital signature of the file. Be suspicious
+* Extracts to current directory
+
+## Fixes
+Are welcome! Please PR me
+
+## License 
+GPLv3. See LICENSE file
---
+++ extract.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euo pipefail
+SRC_FILENAME="$1"
+
+[ -n "$SRC_FILENAME" ] || (echo "File $SRC_FILENAME missing"; exit 1)
+
+# Confirm
+read -p "We DO NOT verify the signature. Are you OK with this? [y/N]: " -n 1 -r
+echo;
+if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "Quitting";
+  exit 2
+fi
+
+xml_extract() {
+  XML_BASE_PATH='.DocumentEnvelope.SignaturePackage.Signature.Object'
+  XML_MY_PATH="${XML_BASE_PATH}.$1"
+  xq -r "$XML_MY_PATH" "$SRC_FILENAME"
+}
+
+DEST_BASENAME="$(xml_extract 'DocumentOriginName["#text"]')"
+DEST_EXTENSION="$(xml_extract 'DocumentExtension["#text"]')"
+DEST_FILENAME="${DEST_BASENAME}.${DEST_EXTENSION}"
+(! [ -f "$DEST_FILENAME" ]) || (echo "Filename $DEST_FILENAME already exists"; exit 1)
+echo "Extracting $DEST_FILENAME"
+xml_extract 'DocumentContent["#text"]' | base64 --decode >"$DEST_FILENAME"
